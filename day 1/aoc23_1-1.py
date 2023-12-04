5f79782b28d76def760bc6162ab4384f117dc4d3
Jan <hentschelj08@gmail.com>
2023-12-04 21:07:08 +0100

    working day 1.1

--- day 1/aoc23_1-1.py
+++ day 1/aoc23_1-1.py
@@ -1,3 +1,16 @@
-with open('input.txt', "r") as input:
+digits = ["0","1","2","3","4","5","6","7","8","9"]
+with open('.\day 1\input.txt', "r") as input:
+    sum = 0
     lines = input.readlines()
-    print(lines[0])
+    for line in lines:
+        firstDigit = ""
+        lastDigit = ""
+        first = True
+        for char in line:
+            if char in digits:
+                if first == True:
+                    firstDigit = char
+                    first = False
+                lastDigit = char
+        sum+=int(firstDigit+lastDigit)
+    print(sum)
