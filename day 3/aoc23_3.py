3bede9a2b7e69e8e7e60e6c88bd41494b32e6b6e
Jan <hentschelj08@gmail.com>
2023-12-06 11:52:20 +0100

    day 3 part 1 solved

--- day 3/aoc23_3.py
+++ day 3/aoc23_3.py
@@ -1,4 +1,5 @@
 import re
+sum = 0
 array2d = []
 symbolList = []
 numberList = []
@@ -7,10 +8,10 @@ with open(".\day 3\input.txt", "r") as input:
     for lineIndex,line in enumerate(lines):
         array2d.append(line)
 
-        symbolIterObject = re.finditer("[*|#|+|$]", line)
+        symbolIterObject = re.finditer("[*|#|+|$|/|@|&|%|=|-]", line)
         
         for x in symbolIterObject:
-            symbolList.append([lineIndex,x.start(), x.group()])
+            symbolList.append([lineIndex,x.start()])
 
         numberIterObject = re.finditer("\d+", line)
         
@@ -18,6 +19,18 @@ with open(".\day 3\input.txt", "r") as input:
             numberList.append([lineIndex,(x.start(), x.end()-1), x.group()])
 
         
-#print(array2d[1][38])
-#print(symbolList)
-#print(numberList)
+
+for symbol in symbolList:
+    lineIndex = symbol[0]
+    indexInLine = symbol[1]
+    #all numbers in adjacent and same line
+    for numberObject in numberList:
+        if numberObject[0] == lineIndex-1 or numberObject[0] == lineIndex or numberObject[0] == lineIndex+1:
+            
+            if indexInLine in range(numberObject[1][0]-1, numberObject[1][1]+2):
+                print(symbol, numberObject)
+                sum += int(numberObject[2])
+                
+    
+
+print(sum)
