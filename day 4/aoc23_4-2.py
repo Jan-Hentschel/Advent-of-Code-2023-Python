f3f182d315f9c01f41cb96c3bf6faff113068693
Jan <hentschelj08@gmail.com>
2023-12-06 21:53:52 +0100

    solved day 4-2

--- day 4/aoc23_4-2.py
+++ day 4/aoc23_4-2.py
@@ -1,13 +1,28 @@
-scratchcardAmount=0
+scratchcardAmount=215
+
+def getMatches(scratshcard):
+    matches = 0
+    winningNumbers = scratchcard[10:40].split()
+    currentNumbers = scratchcard[41:116].split()
+    for currentNumber in currentNumbers:
+        if currentNumber in winningNumbers:
+            matches += 1
+    return matches
+
+scratchesAndSolutions = []
+
 with open(".\day 4\input.txt", "r") as input:
     scratchcards=input.readlines()
     for i,scratchcard in enumerate(scratchcards):
-        matches = 0
-        winningNumbers = scratchcard[10:40].split()
-        currentNumbers = scratchcard[41:116].split()
-        for currentNumber in currentNumbers:
-            if currentNumber in winningNumbers:
-                matches += 1
-        for o in range(1,matches+1):
-            scratchcards.append(scratchcards[o])
-        print(matches)
+        scratchesAndSolutions.append([i,getMatches(scratchcard), 1])
+
+
+for object in scratchesAndSolutions:
+    print(object)
+    while object[2] > 0:
+        for i in range(object[1]):
+            scratchesAndSolutions[i+object[0]+1][2]+=1
+            scratchcardAmount+=1
+
+        object[2]-=1
+print(scratchcardAmount)
