459a260874fcc566c3d2c1c09168fd63ca94eb3a
Jan <hentschelj08@gmail.com>
2023-12-10 17:26:55 +0100

    solved day 10 part 1

--- day 10/test.py
+++ day 10/test.py
@@ -1,7 +1,9 @@
 lines = []
-with open(".\day 10\input.txt", "r") as input:
+with open(".\day 10\inptuttest.txt", "r") as input:
     lines = input.readlines()
 lines = [line.replace("\n", "") for line in lines]
+
+numTiles = 0
 
 pipes = {
     "|": ("north", "south"),
@@ -10,6 +12,11 @@ pipes = {
     "J": ("north", "west"),
     "7": ("south", "west"),
     "F": ("south", "east")
+}
+
+newMapDict = {
+    (22,91): ["west", "S", 0]
+              
 }
 
 currentPosition = (22, 90)
@@ -23,6 +30,8 @@ while currentPosition != (22, 91):
         nextDirection = pipes[currentChar][1]
     elif pipes[currentChar][1] == lastDirection:
         nextDirection = pipes[currentChar][0]
+        
+    newMapDict[currentPosition] = [nextDirection, currentChar, steps]
 
     if nextDirection == "south":
         currentPosition = (currentPosition[0] + 1, currentPosition[1])
@@ -38,5 +47,23 @@ while currentPosition != (22, 91):
         lastDirection = "west"
     steps += 1
 
-farthest = steps / 2
-print(farthest)
+for y,line in enumerate(lines):
+    for x,char in enumerate(line):
+        if not((y,x) in newMapDict):
+            isOutside = 0
+            position = [y,x]
+            while position[1]>=0:
+                if tuple(position) in newMapDict:
+                    pipeDir = newMapDict[tuple(position)][0]
+                    if pipeDir == "north":
+                        isOutside -= 1
+                    elif pipeDir == "south":
+                        isOutside += 1
+                position[1]-=1
+            print(isOutside)
+            if isOutside != 0:
+                numTiles += 1
+
+
+
+print(numTiles)
